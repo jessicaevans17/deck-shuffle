9dfe525019028a2477909e9b1ed0955bc70dfbe0
Jessica Evans <jme@mail.usf.edu>
2019-07-16 17:53:52 -0400

    changed javascript

--- public/main.js
+++ public/main.js
@@ -3,40 +3,21 @@ const main = () => {
     document.querySelector('h1.hello-world').textContent = 'Hello, World!'
   }
 }
-const deck = new Array()
 const suits = ["Spades", "Diamonds", "Clubs", "Hearts"]
 const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-
-let arrayShuffle = function (arr) {
-  let newPos
-  let tempPos
-
-  for (let i = arr.length - 1; i > 0; i--) {
-    newPos = Math.floor(Math.random() * (i + 1))
-    tempPos = arr[i]
-    arr[i] = arr[newPos]
-    arr[newPos] = tempPos
-  }
-  return arr
+shuffleCards = () => {
+  let newSuit = suits[Math.floor(Math.random() * suits.length)]
+  console.log(newSuit)
+  let newValue = values [Math.floor(Math.random() * values.length)]
+  console.log(newValue)
+  let addNewSuit = document.createElement ("p")
+  addNewSuit.textContent = newSuit
+  document.querySelector(".suits").appendChild(addNewSuit)
+  let addNewValue = document.createElement("p")
+  addNewValue.textContent = newValue
+  document.querySelector(".values").appendChild(addNewValue)
 }
-
-const shuffleCards = () =>{
-  let suitShuffle = arrayShuffle (suits)
-  for (let i=0; i < suitShuffle.length; i++) {
-    const createSuit= document.createElement ("p")
-    createSuit.textContent = suitShuffle[i]
-    document.querySelector(".suits").appendChild(createSuit)}
-  let valueShuffle = arrayShuffle (values)
-  for (let i=0; i < valueShuffle.length; i++){
-    const createValue = document.createElement ("p")
-    createValue.textContent = valueShuffle[i]
-    document.querySelector(".values").appendChild(createValue)}
-  }
-  
-  
-
-
 document
   .querySelector(".shuffle")
   .addEventListener("click", shuffleCards)
